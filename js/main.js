1ebb4430d1731aab2d7f86e01c78f8c5348fbecb
rolandoandrade <rolandoandradefernandez@gmail.com>
2019-08-18 16:59:50 -0400

    Rectángulos

--- js/main.js
+++ js/main.js
@@ -7,3 +7,7 @@ new Heart(300,300,"#ff78c3",4).draw();
 new Heart(350,300,"#ff3665",4).draw();
 
 new Heart(400,300,"#ff234a",4).draw();
+
+new Rect(20,20,50,50,"#dd9879").draw();
+
+new DRect(60,80,50,70, "#7be9ff", 5).draw();
--- js/shapes.js
+++ js/shapes.js
@@ -23,3 +23,45 @@ class Heart
         ctx.fill();
     }
 }
+
+
+class Rect
+{
+    constructor(x, y, w, h, color)
+    {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.color = color;
+    }
+
+    draw()
+    {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.w, this.h);
+    }
+}
+
+class DRect extends Rect
+{
+    constructor(x, y, w, h, color, emp)
+    {
+        super(x, y, w, h, color);
+        this.emp = emp;
+    }
+
+    draw()
+    {
+        let ltx = this.x + this.emp, lty = this.y, rtx = this.x+this.w, rty = lty,
+            lbx = this.x, lby = lty+this.h, rbx = lbx + this.w - this.emp, rby = lby;
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        moveTo(ltx,lty);
+        ctx.lineTo(rtx,rty);
+        ctx.lineTo(rbx,rby);
+        ctx.lineTo(lbx,lby);
+        ctx.lineTo(ltx,lty);
+        ctx.fill();
+    }
+}

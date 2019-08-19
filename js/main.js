e5711feeac9babb9ada64d0cfdabbc3cee3c83fe
rolandoandrade <rolandoandradefernandez@gmail.com>
2019-08-19 10:49:53 -0400

    Colisiones con plataforma

--- js/main.js
+++ js/main.js
@@ -7,7 +7,9 @@ let h2 = new Heart(350,300,"#ff3665",4);
 
 let h3 = new Heart(400,300,"#ff234a",4);
 
-let p = new Player(50,50);
+let p = new Player(50,250);
+
+let pl = new Platform(50,300,100,20);
 
 function loop()
 {
@@ -15,7 +17,17 @@ function loop()
     /*h1.beat();
     h2.beat();
     h3.beat();*/
+    p.move();
     p.draw();
+    pl.draw();
+    if(pl.collision(p))
+    {
+        p.ground(pl);
+    }
+    else
+    {
+        p.isGrounded=false;
+    }
 }
 
 window.setInterval(loop, 30);
--- js/player.js
+++ js/player.js
@@ -1,6 +1,9 @@
 const HEAD_RADIUS = 12;
 const EMP = 5;
-const SPEED = 10;
+const SPEED = 8;
+const JUMP_SPEED = 20;
+const GRAVITY = 2;
+const PLAYER_HEIGHT = 40;
 class Player
 {
     constructor(x,y)
@@ -9,8 +12,10 @@ class Player
         this.y = y;
         this.vx = 0;
         this.vy = 0;
+        this.box = {l: x-HEAD_RADIUS, t: y - HEAD_RADIUS, r: x+HEAD_RADIUS, b: y + PLAYER_HEIGHT + HEAD_RADIUS};
+        this.isGrounded = false;
         this.head = new Circle(x,y,HEAD_RADIUS,"#ffa078");
-        this.body = new DRect(x-HEAD_RADIUS,y+HEAD_RADIUS+2,2*HEAD_RADIUS,40,"#ffa078", 0);
+        this.body = new DRect(x-HEAD_RADIUS,y+HEAD_RADIUS+2,2*HEAD_RADIUS,PLAYER_HEIGHT,"#ffa078", 0);
         let a = this;
         document.addEventListener("keydown",(e)=>
         {
@@ -21,14 +26,17 @@ class Player
                 case 37:
                     a.vx = -SPEED; break;
                 case 38:
-                    a.vy = -5; break;
+                    if(a.isGrounded)
+                    {
+                        a.vy = -JUMP_SPEED;
+                        a.isGrounded = false;
+                    }
+                    break;
             }
-            a.move();
 
         });
         document.addEventListener("keyup",()=>  {a.vx = 0;a.move()})
     }
-
 
     move()
     {
@@ -38,11 +46,35 @@ class Player
         let emp = EMP*Math.sign(this.vx);
         this.body.setPosition(this.x-HEAD_RADIUS-emp,this.y+HEAD_RADIUS+2);
         this.body.emp = emp;
+        this.box = {l: this.x-HEAD_RADIUS, t: this.y - HEAD_RADIUS, r: this.x+HEAD_RADIUS, b: this.y + PLAYER_HEIGHT + HEAD_RADIUS};
+        this.vy += this.isGrounded?0:GRAVITY;
     }
 
     draw()
     {
         this.head.draw();
         this.body.draw();
+    }
+
+    ground(platform)
+    {
+        this.isGrounded=true;
+        this.y = platform.y - PLAYER_HEIGHT - HEAD_RADIUS ;
+        this.vy=0;
+        this.move();
+    }
+}
+
+class Platform extends Rect
+{
+    constructor(x,y,w,h)
+    {
+        super(x,y , w, h, "#424242");
+    }
+
+    collision(player)
+    {
+        let r = this, b = player.box;
+        return r.x<=b.r&&b.l<=r.x+r.w&&r.y<=b.b&&b.b<=r.y+r.h;
     }
 }

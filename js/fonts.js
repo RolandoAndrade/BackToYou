af1b6e5115d6fab368301df4f2defca258c0aae1
rolandoandrade <rolandoandradefernandez@gmail.com>
2019-08-20 12:31:06 -0400

    Texto

---
+++ js/fonts.js
@@ -0,0 +1,51 @@
+const DURATION = 80;
+
+class Text
+{
+    constructor(text, x = WIDTH/2, y = HEIGHT-30, size = 15,  color = "rgba(255,255,255,1)", align = "center")
+    {
+        this.text = text;
+        this.size = size;
+        this.color = color;
+        this.align = align;
+        this.x = x;
+        this.y = y;
+        this.opacity = 0;
+        this.read = 0;
+        this.exec = true;
+    }
+
+    draw()
+    {
+        ctx.fillStyle = this.color;
+        ctx.font = this.size+'px Arial, sans-serif';
+        ctx.textAlign = this.align;
+        ctx.fillText(this.text, this.x,this.y);
+    }
+
+    show()
+    {
+        if(this.exec)
+        {
+            if(this.read===0)
+            {
+                this.opacity+=0.05;
+            }
+            else if(this.read>DURATION)
+            {
+                this.opacity-=0.05;
+                if(this.opacity<0)
+                {
+                    this.exec = false;
+                }
+            }
+            if(this.opacity>=1)
+            {
+                this.read++;
+            }
+            this.color = this.color.substring(0,16)+","+this.opacity+")";
+            this.draw();
+        }
+
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,20 +1,41 @@
 let canvas = document.getElementById("canvas"), ctx = canvas.getContext("2d");
+const WIDTH = 700, HEIGHT = 400;
 
-
-let h1 = new Heart(100,200,"#ff78c3",4);
-
-let h2 = new Heart(200,200,"#ff3665",4);
-
-let h3 = new Heart(400,300,"#ff234a",4);
-
-let p = new Player(150,200);
 
 let t = 0;
 
+let q =0;
+
+let ex = false;
+
+let h1 = new Heart(350,200,"#fff",2);
+
+let text = new Text("Al principio todo era oscuridad");
+
 function loop()
 {
-    new Rect(0,0,700,400,"#fff").draw();
-    p.draw();
+    new Rect(0,0,700,400,"#222").draw();
+    h1.beat();
+    /*
+    new Circle(350,200,q*10+40 + 3 * Math.sin(t), "rgba(255,255,255,0.3)").draw();
+    new Circle(350,200,q*10+35 + 3 * Math.sin(t), "rgba(255,255,255,0.4)").draw();
+    new Circle(350,200,q*10+30 + 3 * Math.sin(t), "rgba(255,255,255,0.5)").draw();
+    new Circle(350,200,q*10+25 + 3 * Math.sin(t), "rgba(255,255,255,0.6)").draw();
+    new Circle(350,200,q*10+20 + 3 * Math.sin(t), "rgba(255,255,255,0.7)").draw();
+    new Circle(350,200,q*10+15 + 3 * Math.sin(t), "rgba(255,255,255,0.8)").draw();
+    new Circle(350,200,q*10+10 + 3 * Math.sin(t), "rgba(255,255,255,0.9)").draw();
+    new Circle(350,200,q*10+5, "rgba(255,255,255,1)").draw();*/
+    text.show();
+    t=(t+0.1)%Math.PI;
+    if(ex)
+        q++;
+
+    //p.draw();
 }
+
+document.addEventListener("keydown",()=>
+{
+    ex = true;
+})
 
 window.setInterval(loop, 30);
